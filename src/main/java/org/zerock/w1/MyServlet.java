1789b4b6ffe41f2f2b78f44545ea482dd183fc40
woduf2720 <pjy5602@gmail.com>
2023-12-24 01:27:57 +0900

    29p까지 진행

---
+++ src/main/java/org/zerock/w1/MyServlet.java
@@ -0,0 +1,23 @@
+package org.zerock.w1;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "myServlet", value = "/my")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>MyServlet</h1>");
+        out.println("</body></html>");
+    }
+}
